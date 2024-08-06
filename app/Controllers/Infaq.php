7903c2d0fd563b16dca1c504cb592d9640557904
Ali Usman <long_cafe@yahoo.com>
2024-08-07 06:24:38 +0700

    add infaq

--- app/Controllers/Infaq.php
+++ app/Controllers/Infaq.php
@@ -16,14 +16,42 @@ class Infaq extends BaseController
     use ResponseTrait;
     public function index()
     {
-        //
+        $mi = new ModelInfaq();
+        $data = $mi->select('*')->orderBy(['tanggal' => 'DESC'])->findAll();
+        return $this->respond($data);
     }
     public function byId($id = null)
     {
+        $mi = new ModelInfaq();
+        $data = $mi->select('*')->where('id', $id)->first();
+        if(!$data) return $this->fail('Data infaq tidak ditemukan.', 400);
+        return $this->respond($data);
     }
 
     public function new ()
     {
+        $data = [
+            'tanggal' => null, 
+            'kode' => null,
+            'header' => null,
+            'keterangan' => null,
+            'nominal' => 0,
+            'rutin' => true,
+        ];
+        return $this->respond($data);
+    }
+
+    public function add ()
+    {
+        $mi = new ModelInfaq();
+        $tglSekarang = date('Y-m-d');
+        $infaq = $mi->select(' left(tanggal, 10) as tanggal')->where('tanggal', $tglSekarang)->first();
+        $kode;
+        if(!$infaq) {
+            $kode = $tglSekarang.'-001';
+        } elseif ($infaq['tanggal'] == $tglSekarang) {
+            # code...
+        }
         $data = [
             'tanggal' => null, 
             'kode' => null,
